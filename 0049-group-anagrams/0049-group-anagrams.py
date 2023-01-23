834400db32b62e34a54f779863e7c4741f781fa2
devGO <63961781+devGo20@users.noreply.github.com>
2023-01-24 02:04:27 +0900

    Time: 94 ms (94.12%), Space: 17.3 MB (78.10%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
+        print(anagrams)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return list(anagrams.values())
+        
