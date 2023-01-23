3cbd158f0d0646e4c38b67a35fd59e1149afa011
devGO <63961781+devGo20@users.noreply.github.com>
2023-01-24 00:13:21 +0900

    Time: 37 ms (96.88%), Space: 15.6 MB (20.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        str = []
-        for i in s:
-            if i.isalnum():
-                str.append(i.lower())
-        reverseStr = str.copy()
-        reverseStr.reverse()
-        for i in range(len(str)):
-            if str[i] != reverseStr[i]:
-                return False
-        return True
+        s = s.lower()
+        s = re.sub('[^a-z0-9]','',s)
+       
+        return s == s[::-1]
