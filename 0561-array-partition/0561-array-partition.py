db8820f8f04fec2445968928be7632bf6954acf4
devGO <63961781+devGo20@users.noreply.github.com>
2023-02-21 16:14:39 +0900

    Time: 283 ms (61.43%), Space: 16.8 MB (45.14%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,9 +1,3 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        result = 0
-        nums.sort()
-        for i in range(0, len(nums)-1, 2):
-            result += nums[i]
-        
-        
-        return result
+        return sum(sorted(nums)[::2]) # 슬라이싱 구문 사용. 한 칸씩 건너뜀
