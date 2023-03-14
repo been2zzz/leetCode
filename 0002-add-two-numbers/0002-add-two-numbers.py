f25fe410cec1a304c55cef4595f1dcc6cd96c3f7
devGO <63961781+devGo20@users.noreply.github.com>
2023-03-14 13:36:14 +0900

    Time: 61 ms (93.07%), Space: 13.9 MB (76.60%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,40 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # 연결리스트 -> 역순 연결리스트로 만드는 함수
-    def reverseList(self, head: ListNode) -> ListNode:
-        node, prev = head, None
-        
-        while node:
-            next, node.next = node.next, prev
-            prev, node = node, next
-            
-        return prev
-    
-    # 연결리스트 -> 배열로 만드는 함수
-    def toList(self, node: ListNode) -> List:
-        list: List = []
-        while node:
-            list.append(node.val)
-            node = node.next
-        return list
-    
-    # 문자열 -> 역순 연결리스트 변환 함수
-    def reverseLinkedList(self, result: str) -> ListNode:
-        prev: ListNode = None
-        
-        for i in result:
-            node = ListNode(i)
-            node.next = prev
-            prev = node
-        
-        return node
-    
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        a = self.toList(self.reverseList(l1))
-        b = self.toList(self.reverseList(l2))
-        result = int(''.join(map(str,a))) + int(''.join(map(str,b)))
-        return self.reverseLinkedList(str(result))
+        # 전가산기 구현 (CPU)
+        root = head = ListNode(0)        
+        
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+            
+        return root.next
         
     
         
