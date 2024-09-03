2d9e743857c5113ad37d5f18b9fe319854fa01dd
devGO <63961781+devGo20@users.noreply.github.com>
2024-09-03 22:14:39 +0900

    Time: 50 ms (80.90%), Space: 16.7 MB (41.24%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,7 +5,6 @@ class Solution:
         
         for target in s:
             if target in temp:
-                # Remove characters from the beginning of `temp` up to and including the first occurrence of `target`
                 temp = temp[temp.index(target) + 1:]
             temp += target
             max_length = max(max_length, len(temp))
