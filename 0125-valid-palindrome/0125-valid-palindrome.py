9cf992f71e57c4c42da0dd54c5b792d3397b828a
devGO <63961781+devGo20@users.noreply.github.com>
2024-09-26 22:13:37 +0900

    Time: 35 ms (95.15%), Space: 17.8 MB (25.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,11 +5,4 @@ class Solution:
         manipulateStr = s.replace(" ","")
         manipulateStr = re.sub(r'[^A-Za-z0-9\s]', '', manipulateStr)
         manipulateStr = manipulateStr.lower()
-        first = manipulateStr[:len(manipulateStr)//2]
-        if len(manipulateStr) % 2 == 1:
-            second = manipulateStr[len(manipulateStr)//2+1:]
-        else:
-            second = manipulateStr[len(manipulateStr)//2:]
-        print(first, second)
-
-        return first == second[::-1]
+        return manipulateStr == manipulateStr[::-1]
