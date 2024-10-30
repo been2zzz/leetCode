e2003d8efea7064d9db65a2d1535100477234570
devGO <63961781+devGo20@users.noreply.github.com>
2024-10-30 14:28:27 +0900

    Time: 0 ms (100%), Space: 16.6 MB (44.51%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,9 +1,9 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        jewel_list = list(jewels)
+        freqs = collections.Counter(stones)
         count = 0
-        for stone in stones:
-            if stone in jewel_list:
-                count += 1
+
+        for jewel in jewels:
+            count += freqs[jewel]
 
         return count
